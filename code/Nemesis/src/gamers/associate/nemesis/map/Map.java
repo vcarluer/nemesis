dc55f235e774f595631b346793782267760b94eb
Vincent Carluer <vcarluer@gmail.com>
2014-02-20 23:29:11 +0100

    walls

--- code/Nemesis/src/gamers/associate/nemesis/map/Map.java
+++ code/Nemesis/src/gamers/associate/nemesis/map/Map.java
@@ -1,10 +1,7 @@
 package gamers.associate.nemesis.map;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
@@ -12,7 +9,6 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 
 public class Map {
 	private TiledMap map;
@@ -26,8 +22,9 @@ public class Map {
 	public Map(OrthographicCamera camera)
 	{					
 		build();		
+		parse();
 		renderer = new OrthogonalTiledMapRenderer(map);
-		renderer.setView(camera);
+		renderer.setView(camera);		
 	}
 	
 	public void render() {
@@ -36,5 +33,12 @@ public class Map {
 	
 	private void build() {
 		map = new TmxMapLoader().load("data/map0.tmx");
+	}
+	
+	private void parse() {
+		TiledMapTileLayer layer = (TiledMapTileLayer)map.getLayers().get(0);
+		TiledMapTile tile1 = layer.getCell(0, 0).getTile();
+		boolean isWall1 = tile1.getProperties().containsKey("wall");
+		boolean isWall2 = layer.getCell(1, 1).getTile().getProperties().containsKey("wall");		
 	}
 }
